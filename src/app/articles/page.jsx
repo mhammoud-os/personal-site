ce23959dbee7534562c89e04f6b29091a3092e05
Malek Hammoud <362858318@gotvdsb.ca>
2025-02-14 17:55:55 -0500

    Added images

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -2,57 +2,58 @@ import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { getAllArticles } from '@/lib/articles'
 import { formatDate } from '@/lib/formatDate'
+import Image from 'next/image'
 
 function Article({ article }) {
-  return (
-    <article className="md:grid md:grid-cols-4 md:items-baseline">
-      <Card className="md:col-span-3">
-        <Card.Title href={`/articles/${article.slug}`}>
-          {article.title}
-        </Card.Title>
-        <Card.Eyebrow
-          as="time"
-          dateTime={article.date}
-          className="md:hidden"
-          decorate
-        >
-          {formatDate(article.date)}
-        </Card.Eyebrow>
-        <Card.Description>{article.description}</Card.Description>
-        <Card.Cta>Read article</Card.Cta>
-      </Card>
-      <Card.Eyebrow
-        as="time"
-        dateTime={article.date}
-        className="mt-1 hidden md:block"
-      >
-        {formatDate(article.date)}
-      </Card.Eyebrow>
-    </article>
-  )
+    return (
+        <Card as="article" className="flex flex-col md:flex-row">
+            <div className="relative w-full h-48 md:w-48 md:h-48 flex-shrink-0 z-10">
+                <Image
+                    src={article.image}
+                    alt={`Image for ${article.title}`}
+                    className="object-cover"
+                    fill
+                    unoptimized
+                />
+            </div>
+            <div className="flex flex-col justify-between flex-1 p-4">
+                <div>
+                    <Card.Title href={`/articles/${article.slug}`}>
+                        {article.title}
+                    </Card.Title>
+                    <Card.Description>{article.description}</Card.Description>
+                </div>
+                <div className="mt-2 flex items-center justify-between">
+                    <Card.Cta>Read article</Card.Cta>
+                    <Card.Eyebrow as="time" dateTime={article.date} decorate>
+                        {formatDate(article.date)}
+                    </Card.Eyebrow>
+                </div>
+            </div>
+
+        </Card>
+    )
 }
 
 export const metadata = {
-  title: 'Articles',
-  description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+    title: 'Articles',
+    description:
+        'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
 }
 
 export default async function ArticlesIndex() {
-  let articles = await getAllArticles()
+    let articles = await getAllArticles()
 
-  return (
-    <SimpleLayout
-        title="Exploring Software, Robotics, and AI"
-        intro= "Here you'll find my journey through software development, robotics, AI, and the creative projects that bring these fields to life"
-    >
-      <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-        <div className="flex max-w-3xl flex-col space-y-16">
-          {articles.map((article) => (
-            <Article key={article.slug} article={article} />
-          ))}
-        </div>
-      </div>
-    </SimpleLayout>
-  )
+    return (
+        <SimpleLayout
+            title="Exploring Software, Robotics, and AI"
+            intro="Here you'll find my journey through software development, robotics, AI, and the creative projects that bring these fields to life"
+        >
+            <div className="flex max-w-3xl flex-col space-y-16">
+                {articles.map((article) => (
+                    <Article key={article.slug} article={article} />
+                ))}
+            </div>
+        </SimpleLayout>
+    )
 }
